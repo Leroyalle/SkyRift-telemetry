3365cc734ef8eae8fc21e7201d8bf7cda039bb67
Leroyalle <nikolajmelonov@yandex.ru>
2026-06-14 14:44:29 +0300

    feat: init mongoose module and connect with db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
-import { CREATE_EVENT_USE_CASE_PORT } from './events/application/ports/create-event-use-case.port';
-import { EVENT_REPOSITORY_TOKEN } from './events/application/ports/tokens';
-import { CreateEventUseCase } from './events/application/use-cases/create-event.use-case';
-import { EventRepository } from './events/infrastructure/repositories/event.repository';
+import { EventModule } from './events/event.module';
 
 @Module({
-  imports: [],
-  providers: [
-    {
-      provide: EVENT_REPOSITORY_TOKEN,
-      useClass: EventRepository,
-    },
-    {
-      provide: CREATE_EVENT_USE_CASE_PORT,
-      useClass: CreateEventUseCase,
-    },
+  imports: [
+    EventModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow<string>('MONGO_URI'),
+      }),
+    }),
   ],
 })
 export class AppModule {}
