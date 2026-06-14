acfa2732c169d7b8950ec1771bac7d4f006c3487
Leroyalle <nikolajmelonov@yandex.ru>
2026-06-14 20:11:27 +0300

    fix: register event orm entity in module

--- src/events/event.module.ts
+++ src/events/event.module.ts
@@ -1,14 +1,24 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { CREATE_EVENT_USE_CASE_TOKEN } from './application/ports/create-event-use-case.port';
 import { GET_STATISTICS_USE_CASE_TOKEN } from './application/ports/get-statistics-use-case.port';
 import { EVENT_REPOSITORY_TOKEN } from './application/ports/tokens';
 import { CreateEventUseCase } from './application/use-cases/create-event.use-case';
 import { GetStatisticsUseCase } from './application/use-cases/get-statistics.use-case';
+import { EventOrmEntity } from './infrastructure/entities/event.entity';
 import { EventRepository } from './infrastructure/repositories/event.repository';
 import { EventsController } from './presentation/events/events.controller';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: EventOrmEntity.name,
+        schema: EventOrmEntity,
+      },
+    ]),
+  ],
   providers: [
     {
       provide: EVENT_REPOSITORY_TOKEN,
