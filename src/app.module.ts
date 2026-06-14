ab55cee69faf247ec5563acf76f5280321993469
Leroyalle <nikolajmelonov@yandex.ru>
2026-06-14 14:15:22 +0300

    feat: register event repository and use case providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,21 @@
 import { Module } from '@nestjs/common';
 
+import { CREATE_EVENT_USE_CASE_PORT } from './events/application/ports/create-event-use-case.port';
+import { EVENT_REPOSITORY_TOKEN } from './events/application/ports/tokens';
+import { CreateEventUseCase } from './events/application/use-cases/create-event.use-case';
+import { EventRepository } from './events/infrastructure/repositories/event.repository';
+
 @Module({
   imports: [],
+  providers: [
+    {
+      provide: EVENT_REPOSITORY_TOKEN,
+      useClass: EventRepository,
+    },
+    {
+      provide: CREATE_EVENT_USE_CASE_PORT,
+      useClass: CreateEventUseCase,
+    },
+  ],
 })
 export class AppModule {}
