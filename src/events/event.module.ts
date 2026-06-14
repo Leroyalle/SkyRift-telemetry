88df9fc4b5a449e306a7dc4455033cf0583eb5b2
Leroyalle <nikolajmelonov@yandex.ru>
2026-06-14 18:14:01 +0300

    feat: register event controller and providers

--- src/events/event.module.ts
+++ src/events/event.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 
-import { CREATE_EVENT_USE_CASE_PORT } from './application/ports/create-event-use-case.port';
+import { CREATE_EVENT_USE_CASE_TOKEN } from './application/ports/create-event-use-case.port';
+import { GET_STATISTICS_USE_CASE_TOKEN } from './application/ports/get-statistics-use-case.port';
 import { EVENT_REPOSITORY_TOKEN } from './application/ports/tokens';
 import { CreateEventUseCase } from './application/use-cases/create-event.use-case';
+import { GetStatisticsUseCase } from './application/use-cases/get-statistics.use-case';
 import { EventRepository } from './infrastructure/repositories/event.repository';
+import { EventsController } from './presentation/events/events.controller';
 
 @Module({
   providers: [
@@ -12,9 +15,14 @@ import { EventRepository } from './infrastructure/repositories/event.repository'
       useClass: EventRepository,
     },
     {
-      provide: CREATE_EVENT_USE_CASE_PORT,
+      provide: CREATE_EVENT_USE_CASE_TOKEN,
       useClass: CreateEventUseCase,
     },
+    {
+      provide: GET_STATISTICS_USE_CASE_TOKEN,
+      useClass: GetStatisticsUseCase,
+    },
   ],
+  controllers: [EventsController],
 })
 export class EventModule {}
