0c26d6aa3aa5e96e56cbfefeaa8919e74a3cb145
Leroyalle <nikolajmelonov@yandex.ru>
2026-06-14 18:14:49 +0300

    feat: add RMQ microservice connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+import { createRabbitMqConfig } from './infra/broker/infrastructure/config/rabbit.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+
+  app.connectMicroservice(createRabbitMqConfig(config));
+
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
